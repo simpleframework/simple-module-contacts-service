48fcc2b5489197b88374ad949f0e4eb973fc50bf
CHENK <CHENK@CHENK-PC>
2016-01-21 13:20:47 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/module/contacts/ContactsTag.java
+++ src/main/java/net/simpleframework/module/contacts/ContactsTag.java
@@ -1,6 +1,7 @@
 package net.simpleframework.module.contacts;
 
 import net.simpleframework.ado.bean.AbstractTextDescriptionBean;
+import net.simpleframework.ado.bean.IOrderBeanAware;
 import net.simpleframework.common.ID;
 
 /**
@@ -9,9 +10,15 @@ import net.simpleframework.common.ID;
  * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public class ContactsTag extends AbstractTextDescriptionBean {
+public class ContactsTag extends AbstractTextDescriptionBean implements IOrderBeanAware {
 	/* 机构id */
 	private ID orgId;
+
+	/* 统计字段，联系人数 */
+	private int contacts;
+
+	/* 排序字段 */
+	private int oorder;
 
 	public ID getOrgId() {
 		return orgId;
@@ -19,6 +26,24 @@ public class ContactsTag extends AbstractTextDescriptionBean {
 
 	public void setOrgId(final ID orgId) {
 		this.orgId = orgId;
+	}
+
+	@Override
+	public int getOorder() {
+		return oorder;
+	}
+
+	@Override
+	public void setOorder(final int oorder) {
+		this.oorder = oorder;
+	}
+
+	public int getContacts() {
+		return contacts;
+	}
+
+	public void setContacts(final int contacts) {
+		this.contacts = contacts;
 	}
 
 	private static final long serialVersionUID = 2889282054595763648L;
--- src/main/java/net/simpleframework/module/contacts/impl/ContactsContext.java
+++ src/main/java/net/simpleframework/module/contacts/impl/ContactsContext.java
@@ -1,9 +1,15 @@
 package net.simpleframework.module.contacts.impl;
 
 import static net.simpleframework.common.I18n.$m;
+import net.simpleframework.ado.db.DbEntityTable;
 import net.simpleframework.ctx.AbstractModuleContext;
 import net.simpleframework.ctx.Module;
+import net.simpleframework.module.contacts.Contacts;
+import net.simpleframework.module.contacts.ContactsTag;
+import net.simpleframework.module.contacts.ContactsTagR;
 import net.simpleframework.module.contacts.IContactsContext;
+import net.simpleframework.module.contacts.MyContacts;
+import net.simpleframework.module.contacts.MyContactsTag;
 
 /**
  * Licensed under the Apache License, Version 2.0
@@ -12,6 +18,14 @@ import net.simpleframework.module.contacts.IContactsContext;
  *         http://www.simpleframework.net
  */
 public class ContactsContext extends AbstractModuleContext implements IContactsContext {
+
+	protected DbEntityTable[] createEntityTables() {
+		return new DbEntityTable[] { new DbEntityTable(Contacts.class, "sf_contacts"),
+				new DbEntityTable(ContactsTag.class, "sf_contacts_tag"),
+				new DbEntityTable(ContactsTagR.class, "sf_contacts_tagr"),
+				new DbEntityTable(MyContacts.class, "sf_mycontacts"),
+				new DbEntityTable(MyContactsTag.class, "sf_mycontacts_tag") };
+	}
 
 	@Override
 	protected Module createModule() {
